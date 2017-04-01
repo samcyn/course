051719a94d02ff12e98b044f9ac18cbc03d73c27
superirale <superirale@gmail.com>
2017-04-01 20:07:38 +0100

    homepage copy changed

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,10 +10,12 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css">
 
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
 		<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
-		<link rel="stylesheet" type="text/css" href="https://fonts.google.com/?query=nuni&selection.family=Nunito">
+
+		<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 	</head>
 	<body data="index">
 		<div class="wrapper clearfix">
@@ -31,7 +33,7 @@
 										<span class="icon-bar"></span>
 										<span class="icon-bar"></span>
 									</button>
-									<a class="navbar-brand animated" href="/">
+									<a class="navbar-brand animated" href="#">
 										<img src="/assets/img/logo/logo.svg" alt="Switch" class="logo-default">
 									</a>
 								</div>
@@ -39,7 +41,7 @@
 								<!-- Collect the nav links, forms, and other content for toggling -->
 								<div class="collapse navbar-collapse navbar-ex1-collapse">
 									<ul class="nav navbar-nav navbar-right main-nav-list">
-										<li class="active"><a href="#course">Course</a></li>
+										<li class="active"><a href="#course">Courses</a></li>
 										<li class="animated"><a href="#about">About</a></li>
 										<li class="animated"><a href="#faq">FAQ</a></li>
 									</ul>
@@ -50,7 +52,7 @@
 						<div class="shapes clearfix">
 							<div class="col-md-12">
 								<h3 class="title">SWITCH! Immersive Courses</h3>
-								<p class="introduction">It's time to SWITCH! Accelerate your career by taking our<br> immersive courses in web technologies. A good way to start a new<br> career or expand your skill-set.</p>
+								<p class="introduction">It's time to SWITCH! Accelerate your career by taking our<br> immersive courses in web technologies. A good way to start a new<br> career or expand your skill-set.Type something</p>
 							</div>
 							<!--<div class="col-md-6">
 								<div class="thumbnail">
@@ -59,15 +61,13 @@
 							</div>-->
 						</div>
 					</div>
+					<img  src="assets/img/desktop.png" class="pos-abs desktop">
 				</div>
 				<div class="th_1">
 					<form class="form-inline" method="post">
 					  <span>I want to learn </span>
 					  <div class="form-group">
-					    <!--<select class="form-control">
-					    	<option>1</option>
-					    	<option>1</option>
-					    </select>-->
+			
 						{{csrf_field()}}
 					    <select name="course_id" class="selectpicker form-control" title="Choose a course...">
 						  @foreach($courses as $course)
@@ -85,7 +85,7 @@
 			</header>
 			<section class="work clearfix">
 				<div class="container">
-					<h3 class="text-center heading hr-blue">How It Works</h3>
+					<h3 class="text-center heading hr-blue">What you will learn</h3>
 					<br><br>
 					<div class="row">
 						<div class="col-md-4">
@@ -94,30 +94,32 @@
 	                                <img src="assets/img/com.png">
 	                            </div>
 	                            <div class="content">
-	                                <h5>Complete hands-on projects</h5>
-	                                <p>Research in advertising is done in order to produce better advertisements that are more efficient in motivating customers to buy a product or a service.</p>
+	                                <h5>Market driven curriculum</h5>
+	                                <p>Our curricula are developed with leading technology companies home andabroad willing to hire for skills. 
+										We have tailored our content to current and future market need. 
+										You are sure guaranteed you will complete ourcourses with market-ready skills.</p>
 	                            </div>
 	                       </div>
 						</div>
 						<div class="col-md-4">
 							<div class="card">
 	                            <div class="header">
-	                                <img src="/assets/img/chat.png">
+	                                <img src="assets/img/chat.png">
 	                            </div>
 	                            <div class="content">
-	                                <h5>Complete hands-on projects</h5>
-	                                <p>Research in advertising is done in order to produce better advertisements that are more efficient in motivating customers to buy a product or a service.</p>
+	                                <h5>Practical Training session</h5>
+	                                <p>Our faculties are top in the industry and are hands-on in their approach to training. You spend time not ONLY learning the theories but also the practical step by steps of how things are built.</p>
 	                            </div>
 	                       </div>
 						</div>
 						<div class="col-md-4">
 							<div class="card">
 	                            <div class="header">
-	                                <img src="/assets/img/bag.png">
+	                                <img src="assets/img/bag.png">
 	                            </div>
 	                            <div class="content">
-	                                <h5>Complete hands-on projects</h5>
-	                                <p>Research in advertising is done in order to produce better advertisements that are more efficient in motivating customers to buy a product or a service.</p>
+	                                <h5>Work on real live project</h5>
+	                                <p>While you study, you will be made to work on live projects to have a feel of how the real world works. This prepares you for the workplace.</p>
 	                            </div>
 	                       </div>
 						</div>
@@ -129,7 +131,6 @@
 					<h3 class="text-center heading hr-white">Our Immersive Courses</h3>
 					<br><br>
 					<div class="row">
-						
 						@foreach($courses as $course)
 						
 						<div class="col-md-3">
@@ -154,7 +155,7 @@
 					<!--navbar begins here-->
 					<ul class="p-l-0">
 						<li>
-							<img src="/assets/img/logo/logo.svg" width="150px">
+							<img src="assets/img/logo/logo.svg" width="150px">
 							<span class="copyright">© 2016 Great Simple All rights reserved</span>
 						</li>
 						<ul class="pull-right">
@@ -175,9 +176,9 @@
 		</div>
 
 		<!-- jQuery -->
-		<script src="/assets/js/jquery.js"></script>
+		<script src="assets/js/jquery.js"></script>
 		<!-- Bootstrap JavaScript -->
-		<script src="/assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>

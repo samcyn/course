33699832c1bbf11c2ea21e123be9f795568eb86c
superirale <superirale@gmail.com>
2017-04-03 15:38:59 +0100

    google analytics

--- resources/views/application/index.blade.php
+++ resources/views/application/index.blade.php
@@ -16,6 +16,19 @@
 		<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
 		<link rel="stylesheet" type="text/css" href="/assets/css/responsive.css">
 		<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+		<script>
+
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new
+		Date();a=s.createElement(o),
+
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-89105955-1', 'auto');
+		ga('send', 'pageview');
+
+		</script>
 	</head>
 	<body data="course">
 		<div class="wrapper clearfix m-b-80">

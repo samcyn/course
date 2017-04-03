cf9b41000d83b89ad76638167aa01154b2444c3f
superirale <superirale@gmail.com>
2017-04-03 18:50:49 +0100

    latest changes

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -5,6 +5,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<link rel="icon" type="image/png" href="/assets/img/fav.jpg"/>
+
 		<title>SWITCH! Immersive Courses</title>
 
 		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
